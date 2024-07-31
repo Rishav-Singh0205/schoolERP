4293648604e2c5ed8d4a46847c7b9dc182b0ed06
Rishav Singh <89840621+Rishav-Singh0205@users.noreply.github.com>
2024-07-31 16:15:58 +0530

    first commit

---
+++ models/studentSchema.js
@@ -0,0 +1,83 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    rollNum: {
+        type: Number,
+        required: true,
+        maxlength: 5
+    },
+    parentName: {
+        type: String,
+        required: true
+    },
+    phoneNum: {
+        type: String,
+        required: true,
+        minlength: 10,
+        maxlength: 10
+    },
+    dateOfBirth: {
+        type: String,
+        required: true,
+    },
+    gender: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    sclassName: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'sclass',
+        required: true,
+    },
+    school: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'admin',
+        required: true,
+    },
+    role: {
+        type: String,
+        default: "Student"
+    },
+    examResult: [
+        {
+            subName: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'subject',
+            },
+            marksObtained: {
+                type: Number,
+                default: 0
+            }
+        }
+    ],
+    attendance: [{
+        date: {
+            type: Date,
+            required: true
+        },
+        status: {
+            type: String,
+            enum: ['Present', 'Absent'],
+            required: true
+        },
+        subName: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'subject',
+            required: true
+        }
+    }]
+});
+
+module.exports = mongoose.model("student", studentSchema);
